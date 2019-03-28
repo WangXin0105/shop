5b706887703ba92d80b12aff8c869d85a7e16f5c
wangxin-joker <2099743754@qq.com>
2019-03-28 22:30:35 +0800

    【添加缓存】搭建RedisCluster集群并在服务层添加缓存和缓存同步

--- shop-content/shop-content-service/src/main/java/cn/edu/nun/content/service/impl/ContentServiceImpl.java
+++ shop-content/shop-content-service/src/main/java/cn/edu/nun/content/service/impl/ContentServiceImpl.java
@@ -1,11 +1,15 @@
 package cn.edu.nun.content.service.impl;
 
+import cn.edu.nun.common.jedis.JedisClient;
+import cn.edu.nun.common.utils.JsonUtils;
 import cn.edu.nun.common.utils.ResultModel;
 import cn.edu.nun.content.service.ContentService;
 import cn.edu.nun.mapper.TbContentMapper;
 import cn.edu.nun.pojo.TbContent;
 import cn.edu.nun.pojo.TbContentExample;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,20 +21,45 @@ public class ContentServiceImpl implements ContentService {
     @Autowired
     private TbContentMapper contentMapper;
 
+    @Autowired
+    private JedisClient jedisClient;
+
+    @Value("${CONTENT_LIST}")
+    private String CONTENT_LIST;
+
     @Override
     public ResultModel addContent(TbContent content) {
         content.setCreated(new Date());
         content.setUpdated(new Date());
         contentMapper.insert(content);
+
+        //缓存同步,删除缓存中对应的数据。
+        jedisClient.hdel(CONTENT_LIST, content.getCategoryId().toString());
         return ResultModel.ok();
     }
 
     @Override
     public List<TbContent> getContentListByCid(long cid) {
+        try {
+            String json = jedisClient.hget(CONTENT_LIST, cid + "");
+            if (StringUtils.isNotBlank(json)) {
+                List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+                return list;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         TbContentExample example = new TbContentExample();
         TbContentExample.Criteria criteria = example.createCriteria();
         criteria.andCategoryIdEqualTo(cid);
         List<TbContent> list = contentMapper.selectByExampleWithBLOBs(example);
+
+        try {
+            jedisClient.hset(CONTENT_LIST, cid + "", JsonUtils.objectToJson(list));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return list;
     }
 
