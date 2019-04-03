7018cfcf26fd39d0d616982369446b5410a78b02
wangxin-joker <2099743754@qq.com>
2019-04-03 15:31:51 +0800

    【缺陷修改】将配置文件和ip修改为本地回环地址并且更改fastDFS服务器为七牛云的对象存储服务器

--- shop-manager-web/src/main/java/cn/edu/nun/controller/PictureController.java
+++ shop-manager-web/src/main/java/cn/edu/nun/controller/PictureController.java
@@ -2,6 +2,14 @@ package cn.edu.nun.controller;
 
 import cn.edu.nun.common.utils.FastDFSClient;
 import cn.edu.nun.common.utils.JsonUtils;
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -9,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,13 +30,45 @@ public class PictureController {
     @RequestMapping(value="/pic/upload", produces=MediaType.TEXT_PLAIN_VALUE+";charset=utf-8")
     @ResponseBody
     public String uploadFile(MultipartFile uploadFile) {
-        try {
+        /*try {
             FastDFSClient fastDFSClient = new FastDFSClient("classpath:conf/client.conf");
             String originalFilename = uploadFile.getOriginalFilename();
             String extName = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
             //得到一个图片的地址和文件名
             String url = fastDFSClient.uploadFile(uploadFile.getBytes(), extName);
             url = IMAGE_SERVER_URL + url;
+            Map result = new HashMap<>();
+            result.put("error", 0);
+            result.put("url", url);
+            return JsonUtils.objectToJson(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Map result = new HashMap<>();
+            result.put("error", 1);
+            result.put("message", "图片上传失败");
+            return JsonUtils.objectToJson(result);
+        }*/
+
+
+        //构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone2());
+        //...其他参数参考类注释
+        UploadManager uploadManager = new UploadManager(cfg);
+        //...生成上传凭证，然后准备上传
+        String accessKey = "FAmPGcK4qgPOBHD1ft2UZ6gw-tXtZZcaaQSKzR-E";
+        String secretKey = "keMDlKjo-B-RnWL00K6-HvdI7dhjK5eLfU-HDWO2";
+        String bucket = "shop";
+        //默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = null;
+        try {
+            byte[] uploadBytes = uploadFile.getBytes();
+            Auth auth = Auth.create(accessKey, secretKey);
+            String upToken = auth.uploadToken(bucket);
+
+            Response response = uploadManager.put(uploadBytes, key, upToken);
+            //解析上传成功的结果
+            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+            String url = "http://ppdevkv3u.bkt.clouddn.com/" + putRet.key;
             Map result = new HashMap<>();
             result.put("error", 0);
             result.put("url", url);
